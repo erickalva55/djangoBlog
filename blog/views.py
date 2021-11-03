1af19d6250cb759ddf9cbe0133e5a0ca79b8a14b
erickalva55 <erickofcb@gmail.com>
2021-11-02 21:53:20 -0400

    master

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,23 @@
+from django.http.response import HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from content.models import Post
 
 
 def welcome(request):
-    return render(request, 'blog/index.html')
+    posts = Post.objects.filter(status=Post.ACTIVE)
+
+    return render(request, 'blog/index.html', {'posts': posts})
 
 
 def about(request):
     return render(request, 'blog/about.html')
+
+
+def robots_txt(request):
+    text = [
+        "User-Agent: *",
+        "Disallow: /admin/",
+    ]
+    return HttpResponse("\n".join(text), content_type="text/plain")
--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("content.urls")),
     path('', include("blog.urls")),
-    path('about', include("blog.urls")),
 ]
